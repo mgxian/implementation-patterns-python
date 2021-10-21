1e2556c4aaa8865276b7d279207df22693a83509
maoguangxian <maoguangxian@actionsky.com>
2021-10-21 16:15:14 +0800

    refactor use thread

--- tests/api/article_test.py
+++ tests/api/article_test.py
@@ -1,10 +1,11 @@
+import time
 import unittest
 import requests
 import uvicorn
 from controller.app import app
 from fastapi import status
 from grappa import expect
-from multiprocessing import Process
+from threading import Thread
 
 host = '127.0.0.1'
 port = 6666
@@ -12,23 +13,28 @@ port = 6666
 
 class TestArticle(unittest.TestCase):
     def setUp(self):
-        self.proc = Process(
+        self.thread = Thread(
             target=uvicorn.run,
             args=(app,),
             kwargs={
                 "host": host,
                 "port": port,
-                "log_level": "info"
             },
             daemon=True
         )
-        self.proc.start()
-
-    def tearDown(self):
-        self.proc.terminate()
+        self.thread.start()
+        time.sleep(0.1)
 
     def test_it_can_create_article(self):
         url = "http://{}:{}{}".format(host, port, "/articles")
         article_request = {'title': 'Fake Article', 'description': 'Description', 'body': 'Something', 'author_id': 1}
         response = requests.post(url, json=article_request)
         expect(response.status_code).to.be.equal(status.HTTP_201_CREATED)
+        got = response.json()
+        expect(got['slug']).to.be.equal('fake-article')
+        expect(got['title']).to.be.equal(article_request['title'])
+        expect(got['description']).to.be.equal(article_request['description'])
+        expect(got['body']).to.be.equal(article_request['body'])
+        expect(got['author_id']).to.be.equal(article_request['author_id'])
+        expect(got['created_at']).not_to.be.equal(None)
+        expect(got['updated_at']).not_to.be.equal(None)
